46010e591a4eed23ed887e31ebb121980b65a3f4
MitraGala <68623182+MitraGala@users.noreply.github.com>
2023-04-07 13:28:13 +1000

    Update staff.py

--- staff.py
+++ staff.py
@@ -298,7 +298,7 @@ async def results(ctx,n=5):
 	if (bot.get_guild(995971208938004560).get_role(995971209294520370) in bot.get_guild(995971208938004560).get_member(ctx.author.id).roles):
 		channel = bot.get_guild(995971208938004560).get_channel(1084889623127392266)
 		buffer = []
-		async for message in channel.history(limit=29):
+		async for message in channel.history(limit=28):
 		    buffer.append(message)
 		buffer.reverse()
 		buffer = buffer[:-4]
