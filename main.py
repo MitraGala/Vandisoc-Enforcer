7d30149edb067a21d01dee771562973755111f93
Wubblu <53357046+Wubblu@users.noreply.github.com>
2023-10-12 15:32:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ async def on_ready():
 def checkAdmin(member):
         guild = bot.get_guild(995971208938004560)
         admin = guild.get_role(995971209294520370)
-	altmin = guild.get_role(995971209294520373)
-	roles = guild.get_member(member.id).roles
+        altmin = guild.get_role(995971209294520373)
+        roles = guild.get_member(member.id).roles
         return (admin in roles) or (altmin in roles)
 
 def checkStaff(member):
