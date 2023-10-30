ba203ff054c447c187d06aa81b546f7ac62506e7
Wubblu <53357046+Wubblu@users.noreply.github.com>
2023-10-30 19:03:26 +1000

    Update fun.py

--- fun.py
+++ fun.py
@@ -476,8 +476,8 @@ async def ask(ctx, *, userimp):
 async def china(ctx, *, userImp):
 	if checkStaff(ctx.author) or ctx.channel.id in botChans or ctx.author.id == 468491395938910228:
 		chinese = []
-		messageCont = translator.translate(userImp, dest='zh-tw')
-		chinese.append(translator.translate(messageCont))
+		messageCont = translator.translate(userImp, dest='zh-tw').text
+		chinese.append(translator.translate(messageCont).text)
 		chinese.append(sinopy.pinyin(messageCont, variant='mandarin'))
 		chinese.append(jyutping.convert(messageCont, tone=False))
 		c = Converter(dialect='north')
